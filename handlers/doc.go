edf2b429dff4ffde3288caf2baafde43a8a83bd4
joshuabl97 <blau.joshua@gmail.com>
2023-07-23 19:23:27 -0400

    addeddocs.go for go-swagger

---
+++ handlers/doc.go
@@ -0,0 +1,22 @@
+// Package classification Products API.
+//
+// the purpose of this application is to provide an application
+// that is using plain go code to define an API
+//
+//
+// Terms Of Service:
+//
+// there are no TOS at this moment, use at your own risk, I take no responsibility
+//
+//     Schemes: http, https
+//     Host: localhost
+//     BasePath: /products
+//     Version: 0.0.1
+//
+//     Consumes:
+//     - application/json
+
+//
+//     Produces:
+//     - application/json
+package handlers
