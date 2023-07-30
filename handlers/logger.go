8039bffd8d0a90a9965b651c3ce60d86fe7e5e13
joshuabl97 <blau.joshua@gmail.com>
2023-07-30 03:21:40 -0400

    added loggerMiddlewareMiddleware

--- handlers/logger.go
+++ handlers/logger.go
@@ -8,7 +8,7 @@ import (
 )
 
 // HandlerLogger logs information about incoming requests
-func HandlerLogger(next http.Handler, logger *zerolog.Logger) http.Handler {
+func HandlerLogger(next http.Handler, l *zerolog.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
@@ -16,18 +16,13 @@ func HandlerLogger(next http.Handler, logger *zerolog.Logger) http.Handler {
 		next.ServeHTTP(w, r)
 
 		// Logging the request details
-		logger.Info().
-			Str("method", r.Method).
-			Str("path", r.URL.Path).
-			Str("remote_addr", r.RemoteAddr).
-			Dur("duration", time.Since(start)).
-			Msg("request handled")
+		loggerMiddlewareMiddleware(start, l, r)
 	})
 }
 
 // LoggingMiddleware accepts a HandlerFunc and a *zerolog.Logger,
 // and returns a new HandlerFunc that logs information about incoming requests.
-func LoggingMiddleware(handler http.HandlerFunc, logger *zerolog.Logger) http.HandlerFunc {
+func LoggingMiddleware(handler http.HandlerFunc, l *zerolog.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
@@ -35,11 +30,16 @@ func LoggingMiddleware(handler http.HandlerFunc, logger *zerolog.Logger) http.Ha
 		handler(w, r)
 
 		// Logging the request details
-		logger.Info().
-			Str("method", r.Method).
-			Str("path", r.URL.Path).
-			Str("remote_addr", r.RemoteAddr).
-			Dur("duration", time.Since(start)).
-			Msg("request handled")
+		loggerMiddlewareMiddleware(start, l, r)
 	}
+}
+
+func loggerMiddlewareMiddleware(start time.Time, l *zerolog.Logger, r *http.Request) {
+	// Logging the request details
+	l.Info().
+		Str("method", r.Method).
+		Str("path", r.URL.Path).
+		Str("remote_addr", r.RemoteAddr).
+		Dur("duration", time.Since(start)).
+		Msg("request handled")
 }
